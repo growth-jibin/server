7fa6fc5d8518b79606155d510fe98b3dc377b9e2
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2021-10-28 23:28:05 +0900

    회원가입 중복확인 및 해시화

--- app.js
+++ app.js
@@ -7,22 +7,47 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
+const crypto = require("crypto");
 require("dotenv").config();
+
 var db;
 //use
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//회원가입
 app.post("/auth/join", (req, res) => {
   try {
-    db.collection("login").insertOne(
-      { nickname: req.body.nickname, password: req.body.password },
+    db.collection("login").findOne(
+      { nickname: req.body.nickname },
       (err, result) => {
         if (err) {
-          res.status(400).send({ message: "실패" });
+          res.status(500).send(err);
+        } else if (result) {
+          res.status(401).send("/Unauthorized");
+        } else {
+          if (req.body.password === req.body.checkpassword) {
+            var password = req.body.password;
+            var cipher = crypto.createCipher("aes-256-ecb", password);
+            cipher.update(password, "utf8");
+            var cipheredpassword = cipher.final("hex");
+            db.collection("login").insertOne(
+              {
+                nickname: req.body.nickname,
+                password: cipheredpassword,
+              },
+              (err, result) => {
+                if (err) {
+                  res.status(500).send("/fail");
+                }
+                res.status(201).send("/");
+              }
+            );
+          } else {
+            res.status(404).send("/fail/404");
+          }
         }
-        res.status(200).send("/");
       }
     );
   } catch (e) {
